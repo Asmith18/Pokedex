6a1c383a7ef02a0d3245c6332fc390330bfd1c05
Asmith18 <Smittysadam@gmail.com>
2022-02-04 10:29:59 -0600

    hi trev'or

--- Pokedex/Pokedex/Controllers/Networking/PokemonNetworkController.swift
+++ Pokedex/Pokedex/Controllers/Networking/PokemonNetworkController.swift
@@ -6,12 +6,11 @@
 //
 
 import Foundation
+import UIKit.UIImage
 
 class PokemonNetworkController {
     
     private static let baseURLString = "https://pokeapi.co"
-    
-    
     
     static func fetchPokemon(name searchTerm: String, completion: @escaping ([Pokemon]?) -> Void) {
         
@@ -36,24 +35,44 @@ class PokemonNetworkController {
                 print("fix me plz and thank you")
                 completion(nil)
                 return
-                
             }
             
             do {
                 
-                guard let topLevelDictionary = try JSONSerialization.jsonObject(with: pokemonData, options: .fragmentsAllowed) as? [String: Any],
-                      let pokemon = topLevelDictionary["pokemon"] as? [[String: Any]]
-            else {
+                if let topLevelDictionary = try JSONSerialization.jsonObject(with: pokemonData, options: .fragmentsAllowed) as? [String: Any] {
                     
-                    print("unable to decerialize")
-                    completion(nil)
-                    return
+                let pokemon = Pokemon(dictionary: topLevelDictionary)
+                    
+                    completion(pokemon)
                 }
-                
             } catch {
                 print("Encountered error when decoding the data")
                 completion(nil)
             }
+        }.resume()
+    }
+    
+    static func getImage(from seachTerm: String, completion: @escaping (UIImage?) -> Void) {
+        guard let baseUrl = URL(string: baseURLString) else {return}
+        
+        var urlComponents = URLComponents.init(url: baseUrl, resolvingAgainstBaseURL: true)
+        
+        guard let finalURL = urlComponents?.url else { return }
+        print(finalURL)
+        
+        URLSession.shared.dataTask(with: finalURL) { data, _, error in
+            
+            if let error = error {
+                print("There was an error fetching the data. The url is \(finalURL), the error is \(error.localizedDescription)")
+                completion(nil)
+            }
+            
+            guard let data = data,
+                  let image = UIImage(data: data)
+            else { completion(nil); return }
+            
+            completion(image)
+            
         }.resume()
     }
 }
--- Pokedex/Pokedex/Controllers/ViewControllers/PokemonViewController.swift
+++ Pokedex/Pokedex/Controllers/ViewControllers/PokemonViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class PokemonViewController: UIViewController {
+    
+   
 
     @IBOutlet weak var pokemonSearchBar: UISearchBar!
     @IBOutlet weak var pokemonIDLabel: UILabel!
@@ -15,8 +17,63 @@ class PokemonViewController: UIViewController {
     @IBOutlet weak var pokemonSpriteImageView: UIImageView!
     @IBOutlet weak var pokemonMovesTableView: UITableView!
     
+    var pokemon: Pokemon? {
+        
+        didSet {
+            updateViews()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        pokemonSearchBar.delegate = self
+    }
+    
+    
+   private func updateViews() {
+        
+        guard let pokemon = pokemon else { return }
+        self.pokemonNameLabel.text = pokemon.name
+       self.pokemonIDLabel.text = "\(pokemon.id)"
+       pokemonMovesTableView.reloadData()
+        
+       PokemonNetworkController.getImage(from: pokemon.spritePath) { image in
+            DispatchQueue.main.async {
+                self.pokemonSpriteImageView.image = image
+            }
+        }
     }
 }// End
+
+extension PokemonViewController: UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "moveCell", for: indexPath)
+        
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "\(pokemonMovesTableView)"
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+     
+        
+        PokemonNetworkController.fetchPokemon(name: searchText) { pokemon in
+            guard let pokemon = pokemon else { return }
+        }
+    }
+    
+    
+}
 

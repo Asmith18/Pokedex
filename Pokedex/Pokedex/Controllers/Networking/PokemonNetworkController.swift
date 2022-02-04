31dcc3dc21505b0f4a83060be78ffd10643d9724
Asmith18 <Smittysadam@gmail.com>
2022-02-03 19:48:54 -0600

    images time

--- Pokedex/Pokedex/Controllers/Networking/PokemonNetworkController.swift
+++ Pokedex/Pokedex/Controllers/Networking/PokemonNetworkController.swift
@@ -24,5 +24,36 @@ class PokemonNetworkController {
         guard let finalURL = urlComponents?.url else { return }
         print(finalURL)
         
+        URLSession.shared.dataTask(with: finalURL) { data, _, error in
+            
+            if let error = error {
+                print("There was an error fetching the data. The url is \(finalURL), the error is \(error.localizedDescription)")
+                completion(nil)
+                return
+            }
+            
+            guard let pokemonData = data else {
+                print("fix me plz and thank you")
+                completion(nil)
+                return
+                
+            }
+            
+            do {
+                
+                guard let topLevelDictionary = try JSONSerialization.jsonObject(with: pokemonData, options: .fragmentsAllowed) as? [String: Any],
+                      let pokemon = topLevelDictionary["pokemon"] as? [[String: Any]]
+            else {
+                    
+                    print("unable to decerialize")
+                    completion(nil)
+                    return
+                }
+                
+            } catch {
+                print("Encountered error when decoding the data")
+                completion(nil)
+            }
+        }.resume()
     }
 }
